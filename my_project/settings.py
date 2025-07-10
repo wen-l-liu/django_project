a9d6519d608dbb82cf9a44c5d249f485e44c713b
wen-l-liu <w.l.liu180@gmail.com>
2025-07-10 22:06:05 +0100

    Add Whitenoise to requirements

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wg(-*!t^*_t!uzy7^lj-j!8(lh(e6syt1+2qz05e2eomghpctp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost','127.0.0.1',]
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1',]
 
 
 # Application definition
