1b0dcf8a2e9116321913d98111bddc39194eb64c
wen-l-liu <w.l.liu180@gmail.com>
2025-07-10 18:29:17 +0100

    POST requests and return appropriate responses

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+       return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(f'{request.method} request received. This is the hello world page!')
+
+
     return HttpResponse("Hello, World!")
